ef029ada156b597adc01cda8b13e75663f817b1e
hongfaqiu <951142905@qq.com>
2023-07-03 12:44:34 +0800

    chore: vite config

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 import cesium from 'vite-plugin-cesium';
 export default defineConfig({
-  plugins: [cesium()],
-  optimizeDeps: {
-    exclude: ['mvt-basic-render'],
-  },
+  plugins: [cesium()]
 });
