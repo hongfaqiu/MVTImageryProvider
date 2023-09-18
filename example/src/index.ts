baf9eedef5e80ac33e9b783e1870eedc6b9f696d
hongfaqiu <951142905@qq.com>
2023-09-18 14:48:23 +0800

    fix: overscaledZ undefined

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,8 +1,11 @@
-import { ArcGisMapServerImageryProvider, Viewer } from 'cesium';
+import { ArcGisMapServerImageryProvider, ImageryLayer, Viewer } from 'cesium';
 import './index.css';
 import MVTImageryProvider from 'mvt-imagery-provider';
 
 const viewer = new Viewer('cesiumContainer', {
+  baseLayer: ImageryLayer.fromProviderAsync(ArcGisMapServerImageryProvider.fromUrl('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer', {
+    enablePickFeatures: false
+  }), {}),
   baseLayerPicker: false,
   animation: false,
   fullscreenButton: false,
@@ -16,12 +19,6 @@ const viewer = new Viewer('cesiumContainer', {
   orderIndependentTranslucency: false,
 });
 
-ArcGisMapServerImageryProvider.fromUrl('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer', {
-  enablePickFeatures: false
-}).then(async imageryProvider => {
-  viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
-  viewer.imageryLayers.addImageryProvider(imageryProvider)
-  const provider: any = await MVTImageryProvider.fromUrl('https://demotiles.maplibre.org/style.json');
-  
-  const imageryLayer = viewer.imageryLayers.addImageryProvider(provider);
-})
+const provider: any = await MVTImageryProvider.fromUrl('https://demotiles.maplibre.org/style.json');
+
+const imageryLayer = viewer.imageryLayers.addImageryProvider(provider);
